7494f01277c431c25b652032a81c8303318cb92b
otakuhqz <saul_henriquez@hotmail.com>
2019-05-18 21:08:15 -0600

    fix backup longclick
    Signed-off-by: otakuhqz <saul_henriquez@hotmail.com>

--- ZimLX/src/org/zimmob/zimlx/preferences/HiddenAppsFragment.kt
+++ ZimLX/src/org/zimmob/zimlx/preferences/HiddenAppsFragment.kt
@@ -1,54 +1,39 @@
 package org.zimmob.zimlx.preferences
 
-import android.content.Context
-import android.content.pm.LauncherActivityInfo
 import android.os.Bundle
-import android.view.*
-import androidx.fragment.app.Fragment
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.launcher3.R
-import com.android.launcher3.compat.LauncherAppsCompat
-import com.android.launcher3.compat.UserManagerCompat
-import org.zimmob.zimlx.HiddenAppsAdapter
+import com.android.launcher3.Utilities
+import org.zimmob.zimlx.ZimAppFilter
 
-class HiddenAppsFragment : Fragment(), HiddenAppsAdapter.Callback {
+class HiddenAppsFragment : RecyclerViewFragment(), SelectableAppsAdapter.Callback {
 
-    private lateinit var installedApps: List<LauncherActivityInfo>
-    private lateinit var adapter: HiddenAppsAdapter
+    private lateinit var adapter: SelectableAppsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return RecyclerView(container!!.context)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val context = view.context
-        val recyclerView = view as RecyclerView
-        installedApps = getAppsList(context).apply { sortBy { it.label.toString().toLowerCase() } }
-        adapter = HiddenAppsAdapter(view.context, installedApps, this)
+    override fun onRecyclerViewCreated(recyclerView: RecyclerView) {
+        val context = recyclerView.context
+        adapter = SelectableAppsAdapter.ofProperty(context,
+                Utilities.getZimPrefs(context)::hiddenAppSet, this, ZimAppFilter(context))
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         recyclerView.adapter = adapter
     }
 
-    override fun setTitle(newTitle: String) {
-        activity!!.title = newTitle
-    }
-
-    private fun getAppsList(context: Context): ArrayList<LauncherActivityInfo> {
-        val apps = ArrayList<LauncherActivityInfo>()
-        val profiles = UserManagerCompat.getInstance(context).userProfiles
-        val launcherAppsCompat = LauncherAppsCompat.getInstance(context)
-        profiles.forEach {
-            apps += launcherAppsCompat.getActivityList(null, it)
+    override fun onSelectionsChanged(newSize: Int) {
+        if (newSize > 0) {
+            activity?.title = "$newSize${getString(R.string.hide_app_selected)}"
+        } else {
+            activity?.title = getString(R.string.hide_apps)
         }
-        return apps
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -58,7 +43,7 @@ class HiddenAppsFragment : Fragment(), HiddenAppsAdapter.Callback {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_reset -> {
-                activity!!.title = adapter.clearSelection()
+                adapter.clearSelection()
                 true
             }
             else -> super.onOptionsItemSelected(item)
