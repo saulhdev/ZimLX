b14a3830ea98af1de70a1478ecb06af90e6f54e9
Saul <henriquez.saul@gmail.com>
2020-06-17 20:04:32 -0600

    fix option panel icons.
    Signed-off-by: Saul <henriquez.saul@gmail.com>

--- ZimLX/src/org/zimmob/zimlx/views/OptionsPanel.kt
+++ ZimLX/src/org/zimmob/zimlx/views/OptionsPanel.kt
@@ -77,8 +77,7 @@ class OptionsPanel(context: Context, attrs: AttributeSet) : LinearLayout(context
 
         layoutParams = (layoutParams as FrameLayout.LayoutParams).also { lp ->
             lp.width = deviceProfile.availableWidthPx
-            //lp.height = (deviceProfile.availableHeightPx * (1 - deviceProfile.workspaceOptionsShrinkFactor)).toInt()
-            lp.height = (deviceProfile.availableHeightPx * (1 - 0.70)).toInt()
+            lp.height = (deviceProfile.availableHeightPx * (1 - deviceProfile.workspaceOptionsShrinkFactor)).toInt()
             lp.bottomMargin = insets.bottom
         }
     }
